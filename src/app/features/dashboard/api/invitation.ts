deef83ae724f02e55942f38785b70b01219cf957
yuj2n <dbwls4595@naver.com>
2026-09-10 16:00:40 +0900

    ✨feat: 초대 목록 중복 조회를 위한 훅 추가

--- src/app/features/dashboard/api/invitation.ts
+++ src/app/features/dashboard/api/invitation.ts
@@ -15,3 +15,14 @@ export const inviteUser = async ({ email, dashboardId }: InvitationRequest) => {
   )
   return response.data
 }
+
+// 초대 목록 조회(중복 체크)
+export const getInvitations = async (dashboardId: number | string) => {
+  const response = await authHttpClient.get(
+    `/${process.env.NEXT_PUBLIC_TEAM_ID}/dashboards/${dashboardId}/invitations`,
+  )
+  return response.data.invitations as {
+    id: number
+    invitee: { email: string }
+  }[]
+}
